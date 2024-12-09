8089d1a8247588c5c62b93d3cea0968728c39635
zulja1 <luka.zuljo@gmail.com>
2024-12-09 23:31:45 +0100

    Add rest of the queries(6-10) and minor adjustments to previous queries

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,6 @@
 -- Ime, prezime, spol (ispisati ‘MUŠKI’, ‘ŽENSKI’, ‘NEPOZNATO’, ‘OSTALO’), ime države i prosječna plaća u toj državi za svakog trenera.
-SELECT t.Name, Surname, Sex, c.Name AS country, c.AverageSalary AS average_salary FROM Trainer t
+SELECT t.Name, Surname, Sex, c.Name AS country, c.AverageSalary AS average_salary 
+FROM Trainer t
 JOIN Country c ON t.CountryId = c.CountryId
 
 -- Naziv i termin održavanja svake sportske igre zajedno s imenima glavnih trenera (u formatu Prezime, I.; npr. Horvat, M.; Petrović, T.).
@@ -14,8 +15,7 @@ ORDER BY s.time DESC
 
 -- Top 3 fitness centra s najvećim brojem aktivnosti u rasporedu
 SELECT c.Name AS fitness_center, COUNT(s.ActivityId) AS number_of_activities
-FROM 
-Schedule S
+FROM Schedule S
 JOIN Activity a ON s.ActivityId = a.ActivityId
 JOIN Trainer t ON t.TrainerId = (
      SELECT TrainerId 
@@ -38,19 +38,70 @@ JOIN ActivityTrainer at ON at.TrainerId = t.TrainerId
 GROUP BY t.TrainerId
 
 -- Imena svih članova koji trenutno sudjeluju na nekoj aktivnosti.
-SELECT p.Name AS ParticipantName, p.Surname AS ParticipantSurname
-FROM 
-ActivityParticipant ap
+SELECT p.Name AS participant_name, p.Surname AS participant_surname, aty.type AS activity, s.Time
+FROM ActivityParticipant ap
 JOIN Participant p ON ap.ParticipantId = p.ParticipantId
 JOIN Schedule s ON ap.ActivityId = s.ActivityId
+JOIN Activity a ON a.ActivityId = s.ActivityId
+JOIN ActivityType aty ON aty.ActivityTypeId = a.TypeId 
 WHERE s.Time <= CURRENT_TIMESTAMP AND s.Time + INTERVAL '1 hour' > CURRENT_TIMESTAMP; --Ode sam pretpostavia da aktivnost traje 1 sat ali onda postoji mogucnost da nema trenutnih aktivnosti u tijeku
 
 -- Sve trenere koji su vodili barem jednu aktivnost između 2019. i 2022.
+SELECT DISTINCT t.TrainerId, Name, Surname 
+FROM Trainer t
+JOIN ActivityTrainer at ON at.TrainerId = t.TrainerId
+JOIN Schedule s ON s.ActivityId = at.ActivityId
+WHERE s.Time BETWEEN '2019-01-01' AND '2022-12-31'
 
 -- Prosječan broj sudjelovanja po tipu aktivnosti po svakoj državi.
+SELECT c.Name AS country_name, at.Type AS activity, ROUND(AVG(ap.ParticipantCount), 3) AS average_participation
+FROM Country c
+CROSS JOIN ActivityType at
+JOIN Trainer t ON t.CountryId = c.CountryId
+JOIN ActivityTrainer atr ON t.TrainerId = atr.TrainerId
+JOIN Activity a ON atr.ActivityId = a.ActivityId AND a.TypeId = at.ActivityTypeId
+JOIN(SELECT ActivityId, COUNT(ParticipantId) AS ParticipantCount
+     FROM ActivityParticipant
+     GROUP BY ActivityId) ap ON a.ActivityId = ap.ActivityId
+	 
+GROUP BY c.Name, at.Type
+ORDER BY c.Name, at.Type;
 
 -- Top 10 država s najvećim brojem sudjelovanja u injury rehabilitation tipu aktivnosti
+SELECT c.Name AS country_name, at.Type AS activity, SUM(ap.ParticipantCount) AS total_participation
+FROM Country c
+JOIN Trainer t ON t.CountryId = c.CountryId
+JOIN ActivityTrainer atr ON t.TrainerId = atr.TrainerId
+JOIN Activity a ON atr.ActivityId = a.ActivityId
+JOIN ActivityType at ON a.TypeId = at.ActivityTypeId
+JOIN (SELECT ActivityId, COUNT(ParticipantId) AS ParticipantCount
+     FROM ActivityParticipant
+     GROUP BY ActivityId) ap ON a.ActivityId = ap.ActivityId
+	 
+WHERE at.Type = 'Injury rehabilitation'
+GROUP BY c.Name, at.Type
+ORDER BY total_participation DESC
+LIMIT 10;
 
 -- Ako aktivnost nije popunjena, ispiši uz nju “IMA MJESTA”, a ako je popunjena ispiši “POPUNJENO”
+SELECT S.ActivityId,
+CASE 
+    WHEN (SELECT COUNT(*) 
+         FROM ActivityParticipant 
+         WHERE ActivityId = s.ActivityId) < s.Capacity THEN 'Ima mista'
+    ELSE 'Popunjeno'
+END AS Availability
+FROM Schedule s;
 
 -- 10 najplaćenijih trenera, ako po svakoj aktivnosti dobije prihod kao brojSudionika * cijenaPoTerminu
+SELECT t.TrainerId, Name, Surname, SUM(ap.ParticipantCount * a.Price) AS total_profit
+FROM Trainer t
+JOIN ActivityTrainer at ON t.TrainerId = at.TrainerId
+JOIN Activity a ON at.ActivityId = a.ActivityId
+JOIN (SELECT ActivityId, COUNT(ParticipantId) AS ParticipantCount
+     FROM ActivityParticipant
+     GROUP BY ActivityId) ap ON a.ActivityId = ap.ActivityId
+	 
+GROUP BY t.TrainerId, Name, Surname
+ORDER BY total_profit DESC
+LIMIT 10;
